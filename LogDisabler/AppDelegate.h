fe4388d968f5ed3930b22c959ec01d16aa457170
Michael Mavris <michaelmavris@gmail.com>
2016-09-17 14:43:28 +0300

    Initial Commit

---
+++ LogDisabler/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LogDisabler
+//
+//  Created by Michalis Mavris on 17/09/16.
+//  Copyright © 2016 Miksoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ LogDisabler/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  LogDisabler
+//
+//  Created by Michalis Mavris on 17/09/16.
+//  Copyright © 2016 Miksoft. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
